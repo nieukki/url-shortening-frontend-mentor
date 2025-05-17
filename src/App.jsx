2e2278442ee99197db2908ad9bd0c0e7f959e1e4
Tomasz Pacek <tomekpacek@o2.pl>
2025-05-17 16:36:39 +0200

    Setup of TailwindCSS colors (@theme) and basic website structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import "./App.css";
+import { Header } from "./components/Header.jsx";
+import { Navbar } from "./components/Navbar.jsx";
+import { Form } from "./components/Form.jsx";
+import { ListOfLinks } from "./components/ListOfLinks.jsx";
+import { Footer } from "./components/Footer.jsx";
 
 function App() {
-  return <main>
-    
-  </main>;
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <Navbar />
+      <main>
+        <Form />
+        <ListOfLinks />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
